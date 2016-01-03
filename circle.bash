2b3ec3962a948d64f6527ea87237e28dcff65383
Ben Dalling <b.dalling@locp.co.uk>
2016-01-03 13:31:14 +0000

    WIP

--- circle.bash
+++ circle.bash
@@ -26,23 +26,6 @@ acceptance_tests () {
 
 unit_tests () {
   status=0
-
-  case $CIRCLE_NODE_INDEX in
-    0)  export RVM=1.9.3
-        export PUPPET_GEM_VERSION="~> 3.0"
-        ;;
-    1)  export RVM=2.1.5
-        export PUPPET_GEM_VERSION="~> 3.0"
-        ;;
-    2)  export RVM=2.1.6
-        export PUPPET_GEM_VERSION="~> 4.0"
-        export STRICT_VARIABLES="yes"
-        ;;
-    *)	echo "No tests on this node."
-        return 0
-        ;;
-  esac
-
   rvm use $RVM --install --fuzzy
   export BUNDLE_GEMFILE=$PWD/Gemfile
   rm -f Gemfile.lock
@@ -53,11 +36,31 @@ unit_tests () {
   bundle install --without development
   bundle exec rake lint || status=$?
   bundle exec rake validate || status=$?
+
+  if [ $CIRCLE_NODE_INDEX -gt 2 ]; then
+    echo "No unit tests for this node."
+    return 0
+  fi
+
   bundle exec rake spec SPEC_OPTS="--format RspecJunitFormatter \
       -o $CIRCLE_TEST_REPORTS/rspec/puppet.xml" || status=$?
   return $status
 }
 
 echo "Running on test node $CIRCLE_NODE_INDEX of $CIRCLE_NODE_TOTAL"
+
+case $CIRCLE_NODE_INDEX in
+  0)  export RVM=1.9.3
+      export PUPPET_GEM_VERSION="~> 3.0"
+      ;;
+  1)  export RVM=2.1.5
+      export PUPPET_GEM_VERSION="~> 3.0"
+      ;;
+  2)  export RVM=2.1.6
+      export PUPPET_GEM_VERSION="~> 4.0"
+      export STRICT_VARIABLES="yes"
+      ;;
+esac
+
 $1
 exit $?
